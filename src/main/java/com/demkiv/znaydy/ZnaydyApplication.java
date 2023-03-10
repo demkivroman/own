43ce4e2cff38dce90be62529449d5abb6b069813
Demkiv, Roman [PS] <roman.demkiv@cengage.com>
2023-03-10 13:25:38 +0200

    initial commit

---
+++ src/main/java/com/demkiv/znaydy/ZnaydyApplication.java
@@ -0,0 +1,13 @@
+package com.demkiv.znaydy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZnaydyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZnaydyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/demkiv/znaydy/ZnaydyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demkiv.znaydy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZnaydyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
